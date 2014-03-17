77ad011a591fed2ee87d5a62a4e1cd0ff0a46550
Kentaro Kuwata <kuwaken0925@gmail.com>
2014-03-17 14:41:29 +0900

    Commit 2014.03.17 K. Kuwata

--- Scripts/KibanConvert.sh
+++ Scripts/KibanConvert.sh
@@ -7,7 +7,7 @@ ftype=BldA
 
 for file in `ls ${datadir}FG-JPS-?????-${ftype}*.xml | awk -F / '{print $3}' | awk -F . '{print $1}'`
 do
-  xml=${file}.xml
+  xml=${datadir}${file}.xml
   shp=${outdir}${file}.shp
   ogr2ogr -overwrite -f "ESRI Shapefile" ${shp} ${xml}
   echo Finish Converting ${file}
--- Scripts/KibanMerge.sh
+++ Scripts/KibanMerge.sh
@@ -7,20 +7,20 @@ ftype=BldA
 
 for code in `ls ${datadir}FG-JPS-?????-${ftype}*.xml | awk -F - '{print $3}' | sort | uniq`
 do
-  if [ -e ${outdir}FG-JPS-${code}-*-001/*.shp]; then
+  if [ -e ${outdir}FG-JPS-${code}-${ftype}-*-0001.shp ]; then
     shp=${outdir}${code}.Kiban.Merge
-    cp ${outdir}FG-JPS-${code}*/* .
-    n=`ls ${outdir}FG-HPS-${code}*/*.shp | wc -l`
+    cp ${outdir}FG-JPS-${code}-${ftype}-*.* .
+    n=`ls FG-JPS-${code}-${ftype}-*.shp | wc -l`
     if test ${n} -gt 1; then
-      for f in `ks FG-JPS-${code}-*-0001.* | awk -F . '{print $2}'`
+      for f in `ls FG-JPS-${code}-${ftype}-*-0001.* | awk -F . '{print $2}'`
       do
-        cp FG-JPS-${code}-*-0001.${f} dummy.${f}
+        cp FG-JPS-${code}-${ftype}-*-0001.${f} dummy.${f}
       done
       a=2
       while [ ${a} -le ${n} ]
       do
         p=`expr ${a} | awk '{printf "%04d", $1}'`
-        ogr2ogr -overwrite -update -append FG-JPS-${code}-*-${p}.shp dummy.shp -nln merge.shp
+        ogr2ogr -overwrite -update -append FG-JPS-${code}-${ftype}-*-${p}.shp dummy.shp -nln merge
         for f in `ls merge.* | awk -F '{print $2}'`
         do
           mv merge.${f} dummy.${f}
@@ -31,11 +31,11 @@ do
       do
         mv dummy.${f} ${shp}.${f}
       done
-        rm FG-JPS*
+        rm FG-JPS-* merge.*
       elif test ${n} -eq 1; then
         for f in `ls FG-JPS-${code}*.* | awk -F .'{print $2}'`
         do
-          mv FG-JPS-${code}*.${f} ${shp}.${f}
+          mv FG-JPS-${code}-${ftype}-*.${f} ${shp}.${f}
         done
     fi
   else
